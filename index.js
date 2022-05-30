267405e4a16b0681851a0f58681ad6b193b880ad
Pavel Egorov <egorovpvspb@gmail.com>
2022-05-30 18:39:10 +0300

    исправления по комментариям

--- index.js
+++ index.js
@@ -1,27 +1,28 @@
-function createPromise(val) {
-  // const promise = new Promise((resolve, reject) => {
-  //  resolve(val)
-  // })
-  const promise = Promise.resolve(val);
-  return promise;
-}
+// function createPromise(val) {
+//   return Promise.resolve(val);
+// }
+const createPromise = (value) => Promise.resolve(value);
 
-function createFailedPromise(val) {
-  const promise = Promise.reject(new Error('fail'));
-  return promise;
-}
+// function createFailedPromise(val) {
+//   const promise = Promise.reject(new Error('fail'));
+//   return promise;
+// }
+const createFailedPromise = () => Promise.reject(new Error('fail'));
 
-function waitFor(val) {
-  return val.then(Promise.resolve(val*2))
-}
+// function waitFor(promise) {
+//   return promise.then((val) => val * 2);
+// }
+const waitFor = (promise) => promise.then((value) => value * 2);
 
-function waitForAll() {
-  
-}
+// function waitForAll(promises) {
+//   return Promise.all(promises).then((value) => value.map((el) => el * 3));
+// }
+const waitForAll = (promises) => Promise.all(promises).then((value) => value.map((el) => el * 3));
 
-function waitForFirstOne() {
-  
-}
+// function waitForFirstOne(promises) {
+//   return Promise.race(promises).then((value) => value * 4);
+// }
+const waitForFirstOne = (promises) => Promise.race(promises).then((value) => value * 4);
 
 module.exports = {
   createPromise,
