5dc7d112624e5ed8d8eeb82621200ed33e7e0326
Pavel Egorov <egorovpvspb@gmail.com>
2022-05-30 10:25:16 +0300

    only tests 1-2

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
+function createPromise(val) {
+  // const promise = new Promise((resolve, reject) => {
+  //  resolve(val)
+  // })
+  const promise = Promise.resolve(val);
+  return promise;
+}
+
+function createFailedPromise(val) {
+  const promise = Promise.reject(new Error('fail'));
+  return promise;
+}
+
+function waitFor(val) {
+  return val.then(Promise.resolve(val*2))
+}
+
+function waitForAll() {
+  
+}
+
+function waitForFirstOne() {
+  
+}
 
 module.exports = {
   createPromise,
